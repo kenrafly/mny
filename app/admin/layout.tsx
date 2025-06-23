a7f892826c44a2879906dd0ead8444f87d5a4aab
Wichtra <willy.chairullah@gmail.com>
2025-06-23 20:28:57 +0700

    make Sidebar was responsive for mobile

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,8 +1,7 @@
-import { auth, currentUser } from "@clerk/nextjs/server";
+import AdminClient from "./client";
+import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { ReactNode } from "react";
-import Image from "next/image";
-import { UserButton } from "@clerk/nextjs";
 
 interface AdminLayoutProps {
   children: ReactNode;
@@ -10,31 +9,10 @@ interface AdminLayoutProps {
 
 export default async function AdminLayout({ children }: AdminLayoutProps) {
   const { userId } = await auth();
-  const user = await currentUser();
 
   if (!userId) {
     redirect("/sign-in");
   }
 
-  return (
-    <div className="min-h-screen">
-      <header className="flex justify-between items-center p-4 border-b border-gray-700">
-        <div className="flex items-center gap-3">
-          <Image
-            src="/next.svg"
-            alt="App Logo"
-            width={32}
-            height={32}
-            className="dark:invert"
-          />
-          <h1 className="text-xl font-semibold">Donghua MnY</h1>
-        </div>
-        <div className="flex items-center gap-3">
-          <UserButton />
-          <p>{user?.fullName}</p>
-        </div>
-      </header>
-      <main className="p-4">{children}</main>
-    </div>
-  );
+  return <AdminClient>{children}</AdminClient>;
 }
